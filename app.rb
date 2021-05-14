a9fb6a97f9214b07da6993a33a665f3288c17d38
Sara Evans <saraevs@gmail.com>
2021-05-15 00:37:00 +0100

    added layout boilerplate that uses bootstrap and jquery

--- app.rb
+++ app.rb
@@ -8,9 +8,15 @@ configure :development do
 end
 
 get '/' do
+  @usernames = [ 'ssaunier', 'Papillard' ]
   erb :index
 end
 
 get '/about' do
   erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
